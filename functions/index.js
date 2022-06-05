9869e0b691c05db265ff24fb1889e082a8f6d178
Kamasu Paul <kamasupaul@gmail.com>
2022-06-05 19:31:47 +0300

    -- add cache header

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
 /* eslint-disable linebreak-style */
 const express = require("express");
 const functions = require("firebase-functions");
-const { initializeApp } = require("firebase-admin/app");
-const { getFirestore } = require("firebase-admin/firestore");
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -14,61 +14,76 @@ app.use(cors());
 initializeApp();
 const db = getFirestore();
 
+const setCache = function(req, res, next) {
+  // define period in seconds for cache.
+  const period = 60 * 10080;// 60 * 24 * 7; 1 week
+
+  // you only want to cache for GET requests
+  if (req.method == "GET") {
+    res.set("Cache-control", `public, max-age=${period}`);
+  } else {
+    // for the other requests set strict no caching parameters
+    res.set("Cache-control", "no-store");
+  }
+  next();
+};
+
+// now call the new middleware function in your app
+
+app.use(setCache);
+
 // create index route
 app.get("/", (req, res) => {
   res.send("Hello there!");
 });
 
-//search route
+// search route
 app.get("/search", (req, res) => {
-  //get amount query parameter
+  // get amount query parameter
   const amount = Number(req.query.amount);
   const searchQuery = db.collection("products");
   searchQuery.get().then((querySnapshot) => {
     const products = [];
     querySnapshot.forEach((doc) => {
-      //get transaction_bands array
-      const transaction_bands = doc.data().transaction_bands;
-      //for each transaction_band
-      for (let i = 0; i < transaction_bands.length; i++) {
+      // get transaction_bands array
+      const transactionBands = doc.data().transaction_bands;
+      // for each transaction_band
+      for (let i = 0; i < transactionBands.length; i++) {
         // check if transaction is within the transaction_band range
         if (
-          transaction_bands[i].from <= amount &&
-          transaction_bands[i].to >= amount
+          transactionBands[i].from <= amount &&
+          transactionBands[i].to >= amount
         ) {
-          //create an object and push it to the products array
+          // create an object and push it to the products array
           products.push({
             id: doc.id,
             name: doc.data().name,
-            from: transaction_bands[i].from,
-            to: transaction_bands[i].to,
-            sending_charge: transaction_bands[i].sending_charge,
-            withdraw_charge: transaction_bands[i].withdraw_charge,
+            from: transactionBands[i].from,
+            to: transactionBands[i].to,
+            sending_charge: transactionBands[i].sending_charge,
+            withdraw_charge: transactionBands[i].withdraw_charge,
           });
         }
       }
     });
-    res.send(products);
+    res.status().send(products);
   });
 });
 
 // get all products
 app.get("/products", (req, res) => {
   db.collection("products")
-    .get()
-    .then((snapshot) => {
-      const products = [];
-      snapshot.forEach((doc) => {
-        products.push({
-          id: doc.id,
-          ...doc.data(),
+      .get()
+      .then((snapshot) => {
+        const products = [];
+        snapshot.forEach((doc) => {
+          products.push(doc.data());
         });
+        res.send(products);
+      })
+      .catch((err) => {
+        console.log("Error getting documents", err);
       });
-      res.send(products);
-    })
-    .catch((err) => {
-      console.log("Error getting documents", err);
-    });
 });
 
 // app.listen(4000, () => {
