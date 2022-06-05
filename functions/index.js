2723cf58a2cb75fd56d42f0eeab56d4e26231b3e
Kamasu Paul <kamasupaul@gmail.com>
2022-06-05 18:16:39 +0300

    -- add firebase

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,46 @@
+/* eslint-disable linebreak-style */
+const express = require("express");
 const functions = require("firebase-functions");
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+app.use(bodyParser.json());
+app.use(cors());
+
+// initialize firebase admin
+initializeApp();
+const db = getFirestore();
+
+// create index route
+app.get("/", (req, res) => {
+  res.send("Hello there!");
+});
+
+// get all products
+app.get("/products", (req, res) => {
+  db.collection("products")
+  .get()
+  .then(snapshot => {
+      const products = [];
+      snapshot.forEach(doc => {
+          products.push({
+              id: doc.id,
+              ...doc.data()
+          });
+      });
+      res.send(products);
+  })
+  .catch(err => {
+      console.log("Error getting documents", err);
+  });
+});
+
+// app.listen(4000, () => {
+//   console.log("Server started on port 3000");
 // });
+
+// export functions
+exports.api = functions.https.onRequest(app);
